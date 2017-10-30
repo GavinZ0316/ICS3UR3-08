b57bf68f7c8c4fa5f0c54a80487f41f44613db4c
Gavin Zhou <taifeng.zhou@ocsbstudent.ca>
2017-10-30 06:15:53 -0400

    Init

---
+++ Unit3-08.py
@@ -0,0 +1,11 @@
+# Created by: Gavin Zhou
+# Created on: Oct 23 2017
+# Created for: ICS3U
+
+for number in range(65,91):
+    unicode = unichr(number)
+    print(str(number) + ' -> '+ str(unicode))
+    
+for number in range(97,123):
+    unicode = unichr(number)
+    print(str(number) + ' -> '+ str(unicode))
